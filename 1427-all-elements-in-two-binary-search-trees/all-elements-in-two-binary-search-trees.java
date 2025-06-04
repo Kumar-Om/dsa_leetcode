92638d6a85a03b7dfa240174437cb364155f4184
Om Kumar <145931425+Kumar-Om@users.noreply.github.com>
2025-06-04 05:36:06 +0530

    Time: 17 ms (74.11%) | Memory: 46.3 MB (57.51%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -1,36 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        PriorityQueue<Integer>pq=new PriorityQueue<>();
-        List<Integer>arr=new ArrayList<>();
-        inorder(root1,pq);
-        inorder(root2,pq);
-        while(!pq.isEmpty()){
-
-            arr.add(pq.poll());
-        }
-        return arr;
+        List<Integer> l1=new ArrayList<>();
+        List<Integer> l2=new ArrayList<>();
+        inorder(root1,l1);
+        inorder(root2,l2);
+        return merge(l1,l2);
     }
-
-    public void inorder(TreeNode root,PriorityQueue<Integer>pq){
-        if(root==null)return;
-
-        inorder(root.left,pq);
-        pq.add(root.val);
-        inorder(root.right,pq);
+    public void inorder(TreeNode curr, List<Integer> l){
+        if(curr==null) return;
+        inorder(curr.left, l);
+        l.add(curr.val);
+        inorder(curr.right,l);
+    }
+    public List merge(List<Integer> l1, List<Integer> l2){
+        List<Integer> res=new ArrayList<>();
+        int i=0,j=0;
+        while(i<l1.size() && j<l2.size()){
+            if(l1.get(i) <= l2.get(j)){
+                res.add(l1.get(i));
+                i++;
+            }
+            else{
+                res.add(l2.get(j));
+                j++;
+            }
+        }
+        while(i<l1.size()) res.add(l1.get(i++));
+        while(j<l2.size()) res.add(l2.get(j++));
+        return res;
     }
 }
