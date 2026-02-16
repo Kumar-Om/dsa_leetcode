9878be1b30e0466029e889f271fa38df8af13d31
Om Kumar <145931425+Kumar-Om@users.noreply.github.com>
2026-02-16 23:44:02 +0530

    Time: 206 ms (67.74%) | Memory: 43 MB (49.86%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -2,15 +2,18 @@ class Solution {
     public void solveSudoku(char[][] board) {
         sol(board);
     }
+
     public boolean sol(char board[][]){
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
                 if(board[i][j]=='.'){
-                    for(char num='1';num<='9';num++){
-                        if(isValid(board,i,j,num)){
-                            board[i][j]=num;
-                            if(sol(board))return true;
-                            board[i][j]='.';
+                    for(char c='1';c<='9';c++){
+                        if (isValid(board,c,i,j)){
+                            board[i][j]=c;
+                            if(sol(board)==true)return true;
+                            else{
+                                board[i][j]='.';
+                            }
                         }
                     }
                     return false;
@@ -20,19 +23,22 @@ class Solution {
         return true;
     }
 
-    public boolean isValid(char[][]board,int row,int col,int num){
+    public boolean isValid(char board[][],char num,int row,int col){
         for(int i=0;i<9;i++){
-            if(board[row][i]==num || board[i][col]==num)return false;
+            if(board[row][i]==num)return false;
+
+            if(board[i][col]==num)return false;
         }
 
-        int boxrow=row/3*3;
-        int boxcol=col/3*3;
+        int boxRow=3*(row/3);
+        int boxCol=3*(col/3); 
 
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                if(board[boxrow+i][boxcol+j]==num)return false;
+                if(board[boxRow+i][boxCol+j]==num)return false;
             }
         }
+
         return true;
     }
 }
