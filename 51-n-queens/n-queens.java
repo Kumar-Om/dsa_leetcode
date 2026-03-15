466d0b7de9ff7b18db8e9d7fcbe3f9442f5197c6
Om Kumar <145931425+Kumar-Om@users.noreply.github.com>
2026-03-15 18:05:53 +0530

    Time: 3 ms (43.60%) | Memory: 46.8 MB (41.84%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,30 +1,15 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res= new ArrayList<>();
+        List<List<String>> res=new ArrayList<>();
         char board[][]=new char[n][n];
         for(int i=0;i<n;i++){
             Arrays.fill(board[i],'.');
         }
 
-        solve(0,n,res,board);
+        solve(res,board,0,n);
         return res;
     }
-    public boolean isSafe(int row,int col,int n,char[][]board){
-        for(int j=col;j>=0;j--){
-            if(board[row][j]=='Q')return false;
-        }
-
-        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
-            if(board[i][j]=='Q')return false;
-        }
-
-        for(int i=row,j=col;i<n && j>=0;i++,j--){
-            if(board[i][j]=='Q')return false;
-        }
-        return true;
-    }
-
-    public void solve(int col,int n,List<List<String>> res,char board[][]){
+    public void solve(List<List<String>> res,char board[][],int col,int n){
         if(col==n){
             List<String> temp=new ArrayList<>();
             for(int i=0;i<n;i++){
@@ -35,11 +20,30 @@ class Solution {
         }
 
         for(int row=0;row<n;row++){
-            if(isSafe(row,col,n,board)){
+            if(isSafe(board,row,col,n)){
                 board[row][col]='Q';
-                solve(col+1,n,res,board);
+                solve(res,board,col+1,n);
                 board[row][col]='.';
             }
         }
+    }
+
+    public boolean isSafe(char board[][],int row,int col,int n){
+        //same row check
+        for(int j=col;j>=0;j--){
+            if(board[row][j]=='Q')return false;
+        }
+        
+        //upper left diagonal check
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q')return false;
+        }
+
+        //lower left diagonal check
+        for(int i=row,j=col;i<n && j>=0;i++,j--){
+            if(board[i][j]=='Q')return false;
+        }
+
+        return true;
     }
 }
