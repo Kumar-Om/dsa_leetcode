2925d13f920fdc4bf34ec88589d24e3d789a3e39
Om Kumar <145931425+Kumar-Om@users.noreply.github.com>
2026-07-20 21:57:03 +0530

    Time: 17 ms (99.90%) | Memory: 49.1 MB (22.28%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -10,18 +10,15 @@ class Solution {
         for(int i=1;i<n;i++){
             nums[i]=nums[i-1]+nums[i];
         }
-
+        //1 3 6  k=3
         for(int i=0;i<n;i++){
             int key=nums[i]-k;
             if(map.containsKey(key)){
-                c=c+map.get(key);
+                c+=map.get(key);
             }
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
-            }
-            else{
-                map.put(nums[i],1);
-            }
+            
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            
         }
         return c;
     }
