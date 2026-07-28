a579894d3b9d82fa7236327349b3d4a591208463
Om Kumar <145931425+Kumar-Om@users.noreply.github.com>
2026-07-29 00:26:06 +0530

    Time: 1 ms (100.00%) | Memory: 46 MB (18.78%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,25 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>();
-        combinationSum(candidates,target,res,temp,0,candidates.length);
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer>temp=new ArrayList<>();
+        combinationSum(candidates,target,0,res,temp,candidates.length);
         return res;
     }
-
-    public void combinationSum(int[] candidates,int target,List<List<Integer>> res,List<Integer> temp,int i,int n){
-        if(i==n){
+    public void combinationSum(int candidates[],int target,int idx,List<List<Integer>> res,List<Integer>temp,int n){
+        if(idx==n){
             if(target==0){
                 res.add(new ArrayList<>(temp));
             }
             return;
         }
 
-        if(candidates[i]<=target){
-            temp.add(candidates[i]);
-            combinationSum(candidates,target-candidates[i],res,temp,i,n);
+        if(candidates[idx]<=target){
+            temp.add(candidates[idx]);
+            combinationSum(candidates,target-candidates[idx],idx,res,temp,n);
             temp.remove(temp.size()-1);
         }
 
-        combinationSum(candidates,target,res,temp,i+1,n);
+        combinationSum(candidates,target,idx+1,res,temp,n);
     }
 }
