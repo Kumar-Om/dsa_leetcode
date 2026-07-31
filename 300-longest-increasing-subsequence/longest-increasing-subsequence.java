afc0eecf275011142f94bff5c16eaa92b0c18076
Om Kumar <145931425+Kumar-Om@users.noreply.github.com>
2026-07-31 13:56:46 +0530

    Time: 373 ms (12.44%) | Memory: 237.5 MB (7.51%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -3,7 +3,7 @@ class Solution {
         int n=nums.length;
         int dp[][]=new int[n+1][n+1];
         for(int i=0;i<=n;i++)Arrays.fill(dp[i],-1);
-        for(int i=0;i<=n;i++)dp[n][i]=0;
+        //for(int i=0;i<=n;i++)dp[n][i]=0;
 
         return lis(nums,nums.length,0,-1,dp);
     }
