33be47e1da3f86b2b24c9c06fb4aa27ad4efb9bb
Om Kumar <145931425+Kumar-Om@users.noreply.github.com>
2026-08-15 15:23:44 +0530

    Time: 5 ms (40.66%) | Memory: 46.7 MB (79.92%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,39 +1,32 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    class ListNodeComparator implements Comparator<ListNode>{
-        public int compare(ListNode a,ListNode b){
-            return a.val-b.val;
-        }
-    }
+    public ListNode mergeKLists(ListNode[] lists) {
 
-  public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode>pq=new PriorityQueue<>(new ListNodeComparator());
-        ListNode dummy=new ListNode(-1);
-        ListNode temp=dummy;
+        PriorityQueue<ListNode> pq =
+            new PriorityQueue<>((a, b) -> a.val - b.val);
 
-        for(int i=0;i<lists.length;i++){
-            if(lists[i]!=null){
-                pq.offer(lists[i]);
+        // Put EVERY node into the PriorityQueue
+        for(int i = 0; i < lists.length; i++) {
+
+            ListNode temp = lists[i];
+
+            while(temp != null) {
+                pq.offer(temp);
+                temp = temp.next;
             }
         }
 
-        while(!pq.isEmpty()){
-            ListNode curr=pq.poll();
-            temp.next=curr;
-            temp=temp.next;
-            if(curr.next!=null){
-                pq.offer(curr.next);
-            }
+        // Create new sorted Linked List
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while(!pq.isEmpty()) {
+
+            ListNode node = pq.poll();
+
+            curr.next = new ListNode(node.val);
+            curr = curr.next;
         }
+
         return dummy.next;
     }
 }
